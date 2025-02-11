ec00dae5b444d25d231a21e9fe4a42ad3279bda9
Hazhir2002 <HazhirYousefi2002@gmail.com>
2025-02-11 17:54:26 +0330

    made dashboard the home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 
 import {
   Dashboard,
@@ -16,6 +16,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route index element={<Navigate replace to="dashboard" />} />
         <Route path="dashboard" element={<Dashboard />} />
         <Route path="bookings" element={<Bookings />} />
         <Route path="cabins" element={<Cabins />} />
